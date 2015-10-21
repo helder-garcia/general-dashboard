ecbd701f40b06735737e47095cb5e8c8a8b5a601
Helder Luiz Bezerra de Oliveira Garcia <helder.garcia@serpro.gov.br>
2015-10-21 13:56:04 -0200

    Chart.js e angular-chart incluidos.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute']);
+var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute', 'chart.js']);
 app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/dashboard', {
 		templateUrl : 'assets/html/dashboard.html',
@@ -19,5 +19,13 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
- 
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  }; 
 }]);
