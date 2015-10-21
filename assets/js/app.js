7c9ab86256c1bee07d4ef9cc8ccc86fe377b75a2
Helder Luiz Bezerra de Oliveira Garcia <helder.garcia@serpro.gov.br>
2015-10-21 17:00:16 -0200

    Widgets de exemplo no dashboard.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute', 'chart.js']);
 app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/dashboard', {
 		templateUrl : 'assets/html/dashboard.html',
-		controller : 'NodeListController'
+		controller : 'dashboardCtrl'
 	}).when('/nodes', {
 		templateUrl : 'assets/html/node-list.html',
 		controller : 'NodeListController'
@@ -10,7 +10,7 @@ app.config([ '$routeProvider', function($routeProvider) {
 		templateUrl : 'partials/node-detail.html',
 		controller : 'NodeDetailCtrl'
 	}).when('/', {
-		templateUrl : 'assets/html/dashboard.html'
+		redirectTo : '/dashboard'
 	}).otherwise({
 		redirectTo : '/dashboard'
 	});
@@ -19,13 +19,4 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  }; 
 }]);
---
+++ assets/js/dashboard.controller.js
@@ -0,0 +1,26 @@
+var app = angular.module('StarterApp');
+app.controller('dashboardCtrl', ['$scope', function($scope){
+	}]);
+app.controller('dashboardWidget01Ctrl', ['$scope', function($scope){
+	  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	  $scope.series = ['Series A', 'Series B'];
+	  $scope.data = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	  ];
+	  $scope.onClick = function (points, evt) {
+	    console.log(points, evt);
+	  }; 
+	}]);
+app.controller('dashboardWidget02Ctrl', ['$scope', function($scope){
+    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+    $scope.data = [300, 500, 100, 40, 120];
+	}]);
+app.controller('dashboardWidget03Ctrl', ['$scope', function($scope){
+	 $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+	  $scope.series = ['Series A', 'Series B'];
+	  $scope.data = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	  ];
+	}]);
